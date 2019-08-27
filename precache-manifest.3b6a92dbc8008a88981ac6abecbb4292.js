ad6220a687f08dc3466dc91153836a4131692de7
zfletch <zfletch2@gmail.com>
2019-08-27 14:21:15 -0400

    Updates

---
+++ precache-manifest.3b6a92dbc8008a88981ac6abecbb4292.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f53108568f0031e54c501b2d71071dd7",
+    "url": "/gorman-trees/index.html"
+  },
+  {
+    "revision": "c2f05b2ffc6f7fa952da",
+    "url": "/gorman-trees/static/css/2.be8e0034.chunk.css"
+  },
+  {
+    "revision": "ffa963075b2b99008bcc",
+    "url": "/gorman-trees/static/css/main.a730fb68.chunk.css"
+  },
+  {
+    "revision": "c2f05b2ffc6f7fa952da",
+    "url": "/gorman-trees/static/js/2.f4c67679.chunk.js"
+  },
+  {
+    "revision": "ffa963075b2b99008bcc",
+    "url": "/gorman-trees/static/js/main.2b08a22f.chunk.js"
+  },
+  {
+    "revision": "fb971786dbdec445e6da",
+    "url": "/gorman-trees/static/js/runtime~main.8be5d4ba.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/gorman-trees/precache-manifest.f88337aa9734d125b2f3efee0f93c8c4.js"
+  "/gorman-trees/precache-manifest.3b6a92dbc8008a88981ac6abecbb4292.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/gorman-trees/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
